38cc595d6ce6428c251648845889589b63cb7cbf
vanessajmh <vanessajmhanna@gmail.com>
2024-10-10 11:35:15 +0200

    minor modif

--- tmp_p_infer.sh
+++ tmp_p_infer.sh
@@ -59,7 +59,7 @@ for file in "$input_dir"/*.csv; do
     echo "Processing $file"
     # Run tcrpeg infer on the file
     sample_name=$(basename "$file" .csv)
-    python i3_scripts/tcrpeg_p_infer.py \
+    python3 i3_scripts/tcrpeg_p_infer.py \
       --input "$file" \
       --output "$output_dir" \
       --device "${device:-cpu}" \
